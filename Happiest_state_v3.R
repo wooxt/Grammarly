03b5b37b61361697663117d8159001c0640f944e
Kirill Teplyakov <kiriltepl@gmail.com>
2015-05-22 23:54:17 +0300

    Update Happiest_state_v3.R

--- Happiest_state_v3.R
+++ Happiest_state_v3.R
@@ -23,7 +23,12 @@ happiness <- function(sentiment.file = "D:/Study/data-engineer-problem/AFINN-111
   state <- function(states, long, lat) { 
   # function that determines if tweet was written in USA or not
   # compares lantitude & longitude coordinates in tweet with coordinates of each state
-  # Args: states - file with coordinates of each USA state
+  # Args: 
+  # states - file with coordinates of each USA state
+  # long - longitude coordinate of tweet
+  # lat - latitude coordinate of tweet
+  # Returns:
+  # state - character, name of state or "Not USA" value
     for (l in 1:nrow(states)) { 
       if (states[l, 5] < lat & lat < states[l, 4] & states[l, 3] > long & long > states[l, 2]) { 
         state <- states[l,1]
@@ -37,6 +42,12 @@ happiness <- function(sentiment.file = "D:/Study/data-engineer-problem/AFINN-111
   }
 
   happiness <- function (text, words){
+  # function that computes sentiments of tweet
+  # Args:
+  # text - text of tweet
+  # words - file contains sentiment words with valents
+  # Returns:
+  # happiness - numeric, contains sum of sentiment in tweet
     x <- 0
     for (y in 1:nrow(words)) {
       if (grepl(words[y, 1], text)) {
@@ -50,7 +61,7 @@ happiness <- function(sentiment.file = "D:/Study/data-engineer-problem/AFINN-111
     return (happiness)
   }
 
-  data <- readLines(tweet.path)
+  data <- readLines(tweet.path) 
   words <- readChar(sentiment.file, nchars = 100000000)
   states <- read.table("D:/Program Files/R/Intern/states.txt", header = TRUE, stringsAsFactors = FALSE)
 
@@ -68,29 +79,29 @@ happiness <- function(sentiment.file = "D:/Study/data-engineer-problem/AFINN-111
 
   j <- 1
   for (i in 1:length(data)) {
-    json_data <- fromJSON(data[i], simplifyVector = FALSE)
+    json_data <- fromJSON(data[i], simplifyVector = FALSE) # takes line from data and convert it from JSON to list
 
-    if (!is.null(json_data$coordinates) & is.null(json_data$delete)) {
-      lat <- json_data$coordinates[2]$coordinates[2]
-      long <- json_data$coordinates[2]$coordinates[1]
+    if (!is.null(json_data$coordinates) & is.null(json_data$delete)) { # if data not delete and has not null coordinates, than proceed
+      lat <- json_data$coordinates[2]$coordinates[2]  # write latitude & longitude coordinates of tweet
+      long <- json_data$coordinates[2]$coordinates[1] 
 
-      if (min(states[ ,5]) < lat & lat < max(states[ ,4]) & max(states[ ,3]) > long & long > min(states[ ,2])) {
-        text[j] <- json_data$text
-        language[j] <- json_data$lang
-        latitude[j] <- json_data$coordinates[2]$coordinates[2]
-        longitude[j] <- json_data$coordinates[2]$coordinates[1]
-        usa.state[j] <- state(long = longitude[[j]], lat = latitude[[j]], states = states)
-        happiness.lvl[j] <- happiness(words = words, text = text[j])
-        j <- j+1
+      if (min(states[ ,5]) < lat & lat < max(states[ ,4]) & max(states[ ,3]) > long & long > min(states[ ,2])) { # compare coordinates with USA boundary coordinates
+        text[j] <- json_data$text # tweet text
+        language[j] <- json_data$lang # tweet language
+        latitude[j] <- json_data$coordinates[2]$coordinates[2] # tweet latitude
+        longitude[j] <- json_data$coordinates[2]$coordinates[1] # tweet language
+        usa.state[j] <- state(long = longitude[[j]], lat = latitude[[j]], states = states) # compute state belongness
+        happiness.lvl[j] <- happiness(words = words, text = text[j]) # compute sentiment
+        j <- j+1 #counter for iterations
       }
     }
   }
 
-  df <- data.frame(cbind(text, happiness.lvl, usa.state, language), stringsAsFactors = FALSE)
-  df <- df[df$usa.state != "Not USA", ]
-  grp <- group_by(df, usa.state)
-  hap.sum <- summarize(grp, happiness = sum(as.numeric(happiness.lvl)))
-  hap.states <- arrange(hap.sum, desc(happiness))[1,]
+  df <- data.frame(cbind(text, happiness.lvl, usa.state, language), stringsAsFactors = FALSE) # create dataframe
+  df <- df[df$usa.state != "Not USA", ] # filter dataframe with only USA tweets
+  grp <- group_by(df, usa.state) # group variables for states
+  hap.sum <- summarize(grp, happiness = sum(as.numeric(happiness.lvl))) # counting sum of sentiment in each state
+  hap.states <- arrange(hap.sum, desc(happiness))[1,] # arranging sentiment by desc and saving states names
 
-  return (hap.states[1])
+  return (hap.states[1]) # return first value of hap.states vector
 }
